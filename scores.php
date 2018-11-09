69515ef0fc8035a36fb79aac582142756fc0e85a
tcubed <30483704+tcubed@users.noreply.github.com>
2018-11-08 21:18:47 -0600

    opposite scores and timeout memes

--- scores.php
+++ scores.php
@@ -13,36 +13,10 @@ header("Refresh:5");
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>  
-  <style>
-  h1{font-size:50px;}    
-
-#overlay {
-    position: fixed; /* Sit on top of the page content */
-    display: none; /* Hidden by default */
-    width: 100%; /* Full width (cover the whole page) */
-    height: 100%; /* Full height (cover the whole page) */
-    top: 0; 
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: rgba(0,0,0,0.5); /* Black background with opacity */
-    z-index: 2; /* Specify a stack order in case you're using a different order for other elements */
-    cursor: pointer; /* Add a pointer on hover */
-}
-#mycarousel{
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    font-size: 50px;
-    color: white;
-    transform: translate(-50%,-50%);
-    -ms-transform: translate(-50%,-50%);
-}
-  .carousel-inner img {
-      height: 600px;
-  }
-  </style>
-
+  
+  
+  <link rel="stylesheet" type="text/css" href="styles.css">
+  
 <?php
 if(array_key_exists("d",$_GET)==0 || array_key_exists("v",$_GET)==0){
     header("url:index.html");
@@ -80,23 +54,76 @@ function activeButton($thisDiv){
     if($division===$thisDiv){echo "btn-warning";}
     else{echo "btn-secondary";}
 }
-
+if($division=="A"){
+    $otherDivision="B";
+}
+else{
+    $otherDivision="A";
+}
 if($allScores[$division]["timeout"]==0){$timeout="none";}
 else{$timeout="block";}
         
 ?>
+  
 <script>
-    function on() {
+var time0=new Date();
+var s0=time0.getSeconds();
+function on() {
     document.getElementById("overlay").style.display = "block";
 }
-
 function off() {
     document.getElementById("overlay").style.display = "none";
 }
+
+function showSnackBar() {
+    // Get the snackbar DIV
+    var x = document.getElementById("snackbar");
+    // Add the "show" class to DIV
+    x.className = "show";
+    // After 15 seconds, remove the show class from DIV
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 15000);
+}
+function hideSnackBar() {
+    // Get the snackbar DIV
+    var x = document.getElementById("snackbar");
+    // Add the "show" class to DIV
+    x.className =x.className.replace("show", "");
+    // After 15 seconds, remove the show class from DIV
+    //setTimeout(function(){ x.className = x.className.replace("show", ""); }, 15000);
+}
+function startTime(){
+    var today=new Date();
+    var s=today.getSeconds();
+    var m = today.getMinutes();
+    var msg=document.getElementById("msg");    
+    
+    if(m%3==0 & s<15){
+        showSnackBar()
+    }
+    
+    // show timeout slides
+    var e=document.getElementById("timeout");
+    if(e.value=="block"){
+        // timeout is in effect
+        var em=document.getElementById("meme");
+        //em.src=getImage();
+        //msg.innerHTML=s+","+em.src;
+    }
+    var t=setTimeout(startTime,500);
+}
+function getImage(){
+    var today=new Date();
+    var s=today.getSeconds();
+    var idx=Math.floor(s/10)+1;
+    var src="img/meme_"+idx+".jpg";
+    return src;
+}
+
 </script>
 </head>
-<body style="background-color:black">
+<body style="background-color:black" onload="startTime()">
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <input type="hidden" id="timeout" value="<?php echo $timeout;?>">
     <div class="container-fluid">
         <div class="btn-group flex">
             <a class="nav-link btn <?php activeButton("A");?>" href="scores.php?d=WGL&v=A">
@@ -158,22 +185,45 @@ function off() {
 
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-bottom">
     <span class="text-white">F11 for full screen.</span>
+    <span class="text-white" id="msg"></span>
 </nav>
 
-<div id="overlay" style="display:<?php echo $timeout;?>">
+    
+<!-- The actual snackbar -->
+<div id="snackbar">
+    <h1 style="color:#000">Division <?php echo $otherDivision; ?></h1>
+    <div class="card-deck">
+        <div class="card bg-dark">
+            <div class="card-body text-center">
+                <h1><?php echo $allScores[$otherDivision]["team1"]["score"]; ?></h1>
+                <h2><?php echo $allScores[$otherDivision]["team1"]["name"]; ?></h2>
+            </div>
+        </div>
+        <div class="card bg-dark">
+            <div class="card-body text-center">
+                <h1><?php echo $allScores[$otherDivision]["team2"]["score"]; ?></h1>
+                <h2><?php echo $allScores[$otherDivision]["team2"]["name"]; ?></h2>
+            </div>
+        </div>
+        <div class="card bg-dark">
+            <div class="card-body text-center">
+                <h1><?php echo $allScores[$otherDivision]["team3"]["score"]; ?></h1>
+                <h2><?php echo $allScores[$otherDivision]["team3"]["name"]; ?></h2>
+            </div>
+        </div>
+    </div>
+</div>
+    
 
+<div id="overlay" style="display:<?php echo $timeout;?>">
+    <div id="mycarousel">
+        <!--<img style="height:600px" id="meme" src="img/meme_1.jpg" alt="">-->
+        <img style="height:600px" id="meme" src="" alt="">
+    </div>
+    <!--
     <div id="mycarousel" class="carousel slide" data-ride="carousel" 
          data-interval=2500 data-pause=false>
-
-        <!-- Indicators -->
-  <ul class="carousel-indicators">
-    <li data-target="#demo" data-slide-to="0" class="active"></li>
-    <li data-target="#demo" data-slide-to="1"></li>
-    <li data-target="#demo" data-slide-to="2"></li>
-  </ul>
-        
-        
-      <!-- The slideshow -->
+      
       <div class="carousel-inner">
         <div class="carousel-item active">
           <img src="img/meme_1.jpg" alt="">
@@ -185,17 +235,8 @@ function off() {
           <img src="img/meme_3.jpg" alt="">
         </div>
       </div>
-
-      <!-- Left and right controls -->
-  <a class="carousel-control-prev" href="#demo" data-slide="prev">
-    <span class="carousel-control-prev-icon"></span>
-  </a>
-  <a class="carousel-control-next" href="#demo" data-slide="next">
-    <span class="carousel-control-next-icon"></span>
-  </a>
-
     </div>
-
+-->
 </div>
     
 </body>
